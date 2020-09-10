68ab6b0ab774cea1881b46d29bd8ad0bafb99911
Sophie Slaats <sophieslaats@outlook.com>
2020-09-10 17:18:23 +0200

    finished expt

--- stimulus.py
+++ stimulus.py
@@ -1,44 +1,34 @@
-import os
-from os.path import join
-import csv
 from psychopy import visual, sound
 
 class Stimulus:
     def __init__(self,
-                 filename,
-                 path,
+                 id,
                  condition,
                  frequency,
+                 filename,
+                 path,
                  modality):
 
+        self.id = id
         self.filename = filename
         self._path = path
         self.condition = condition
         self.frequency = frequency
         self.modality = modality
 
-    def load_stimuli(self):
-        with open(join(path, "lexdec_stimuli.txt"), "r") as stim_file:
-            stim_reader = csv.reader(stim_file)
+    def preload_stimulus(self, window):
+        self.preload_audio()
+        self.preload_text(window)
 
-            next(stim_reader)
-            stim_dict = dict()
+    def preload_audio(self):
+        if self.condition['rw']:
+            if self.frequency['HF']:
+                self.sound = sound.Sound(f'{self._path}/HF/{self.filename}')
+            elif self.frequency['LF']:
+                self.sound = sound.Sound(f'{self._path}/LF/{self.filename}')
+        elif self.condition['nw']:
+            self.sound = sound.Sound(f'{self._path}/NW/{self.filename}')
 
-            for stim in stim_reader:
-                stim_dict[stim[0]] = stim[1:]
-
-            return stim_dict
-
-   # def preload_audio(self):
-    #    if self.condition['word']:
-     #       if self.frequency['HF']:
-      #          self.sound = sound.Sound(f'{self._path}/HF/{self.filename}')
-       #     elif self.frequency['LF']:
-        #        self.sound = sound.Sound(f'{self._path}/LF/{self.filename}')
-       # elif self.condition['nonword']:
-        #    self.sound = sound.Sound(f'{self._path}/NW/{self.filename}')
-
-    #def preload_text(self, window):
-     #   self.text = str(self.filename)[0:-4]
-      #  self.message = visual.TextStim(window, text=self.text, color=-1)
-
+    def preload_text(self, window):
+        _text = str(self.filename)
+        self.message = visual.TextStim(window, text=_text, color=-1)
--- trial.py
+++ trial.py
@@ -1,31 +1,38 @@
 import psychopy as psy
 
 class Trial:
-    def __init__(self, stimulus, cue, target, delay=0.5, max_target_time=2):
+    def __init__(self, stimulus, condition, frequency, modality, delay=0.5, max_target_time=2):
         self._stimulus = stimulus
-        self._cue = stimulus.__dict__[cue]
-        self._target = stimulus.__dict__[target]
+        self._condition = stimulus.__dict__[condition]
+        self._frequency = stimulus.__dict__[frequency]
+        self._modality = stimulus.__dict__[modality]
         self._delay = delay
         self._max_target_time = max_target_time
 
         self.result = {
-            'name': stimulus.name,
-            'cue': cue,
-            'target': target,
-            'reaction_time ': None,
+            'id': self._stimulus.id,
+            'word': self._stimulus.filename,
+            'condition': condition,
+            'frequency': frequency,
+            'modality': modality,
+            'reaction_time': None,
             'response': None
         }
 
     def run_standard(self, window):
-        self._cue.play()
-        psy.core.wait(self._cue.getDuration())
-        psy.core.wait(self._delay)
+        if self._stimulus.modality['auditory']:
+            self._stimulus.sound.play()
+            psy.core.wait(self._stimulus.sound.getDuration())
+            psy.core.wait(self._delay)
 
-        self._target.draw()
-        window.flip()
+        elif self._stimulus.modality['visual']:
+            self._stimulus.message.draw()
+            psy.core.wait(self._delay)
+            window.flip()
+
         clock = psy.core.Clock()
         keys = psy.event.waitKeys(maxWait=self._max_target_time, keyList=['z', 'm'], clearEvents=True,
                                   timeStamped=clock)
-        self.result['response'], self.result['reaction_time'] = keys[0] if keys is not None and len(keys) > 0 else (
-        None, self._max_target_time)
+        self.result['response'], self.result['reaction_time'] = keys[0] if keys is not None and len(keys) > 0 else \
+            (None, self._max_target_time)
         window.flip()
